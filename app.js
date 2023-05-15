90d4717c3969f6ea854a9cb51451e28040f43869
mayad <mayaell912@gmail.com>
2023-05-14 21:32:43 -0400

    Express website, static HTML only

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const hbs = require('hbs');
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
+var roomsRouter = require('./app_server/routes/rooms');
+var mealsRouter = require('./app_server/routes/meals');
+var newsRouter = require('./app_server/routes/news');
+var aboutRouter = require('./app_server/routes/about');
+var contactRouter = require('./app_server/routes/contact');
+
 var app = express();
 
 // view engine setup
--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,9 +1,9 @@
-/* GET travel view */
-const travel = (req, res) => {
+/* GET Rooms View */
+const rooms = (req, res) => {
     pageTitle = process.env.npm_package_description + " - Rooms"; // process.env.npm_package_description can only be accessed if you use 'npm start' *nodemon will not work
     res.render('rooms', { title: pageTitle });
-   };
-
-   module.exports = {
-    travel
-   };
+  };
+  
+  module.exports = {
+    rooms,
+  };
